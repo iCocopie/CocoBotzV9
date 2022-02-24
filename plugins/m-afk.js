2a84439a306bddbecd5a408f3579c39146d5bdd2
Cocopie <92080735+iCocopie@users.noreply.github.com>
2022-02-24 15:24:03 +0700

    Update m-afk.js

--- plugins/m-afk.js
+++ plugins/m-afk.js
@@ -3,7 +3,7 @@ let handler = async(m, { text }) => {
     user.afk = +new Date
     user.afkReason = text
     m.reply(`
-${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
+「 *AFK* 」\n${conn.getName(m.sender)} Is Now AFK${text ? ' With Reason : ' + text : ''}
 `)
 }
 handler.help = ['afk [alasan]']
